71a1c27fe18e4f44f5dad394aa6a2bc72713805a
luskha12 <lucas.limaacesso@gmail.com>
2024-11-22 17:29:36 -0300

    corrigindo api

--- services/userService.js
+++ services/userService.js
@@ -23,3 +23,17 @@ exports.getEmergencyContact = async (id) => {
   }
   return null;
 };
+
+// Nova função para obter dados do usuário
+exports.getUserById = async (id) => {
+  const query = `
+    SELECT tipoUsuario, nome, cpf, telefone, email, dataNascimento, endereco, cep, numeroEmergencia, diasAtendimento, areaAtuacao, outrosArea
+    FROM amparousers
+    WHERE id = $1
+  `;
+  const result = await pool.query(query, [id]);
+  if (result.rows.length > 0) {
+    return result.rows[0];
+  }
+  return null;
+};
